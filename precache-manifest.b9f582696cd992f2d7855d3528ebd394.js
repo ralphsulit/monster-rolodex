f8cc9e89baf30b4b64412566f9b51f5523461f45
Joseph <ralphlsulit@gmail.com>
2022-08-03 08:48:22 +0800

    Updates

---
+++ precache-manifest.b9f582696cd992f2d7855d3528ebd394.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "424a783adc524c659a66",
+    "url": "/monster-rolodex/static/css/main.410398a7.chunk.css"
+  },
+  {
+    "revision": "424a783adc524c659a66",
+    "url": "/monster-rolodex/static/js/main.424a783a.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/monster-rolodex/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/monster-rolodex/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "73aaabf1172c920162b0",
+    "url": "/monster-rolodex/static/js/runtime~main.73aaabf1.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/monster-rolodex/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d9a655944b70cd31278d8a39e873f555",
+    "url": "/monster-rolodex/index.html"
+  }
+];

24677eae51e701cbd6d2e174b36f873fdc447863
Joseph <ralphlsulit@gmail.com>
2022-08-11 08:53:50 +0800

    added image, name, and email in cardlist component

--- src/Components/card-list/card-list.component.jsx
+++ src/Components/card-list/card-list.component.jsx
@@ -4,12 +4,23 @@ class CardList extends Component {
   
   render() {
     const { monsters } = this.props;
-
+    
     return (
-      <div>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+      <div className='card-list'>
+        {monsters.map((monster) => {
+          const { id, name, email } = monster;
+          
+          return(
+            <div className='card-container' key={id}>
+              <img
+                alt='monster'
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
